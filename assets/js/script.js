6b6ab2e20eb72d87677e8414f3090bb1edc015c5
tech-doc <imchirag2000@gmail.com>
2020-07-14 19:56:52 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+// // let currentValue = "2";
+
+// function badgeHandler() {
+//     let badgeBtn = document.getElementById('badge').innerHTML;
+//     let currentValue = 0;
+//     console.log('working');
+//     badgeBtn = currentValue + 1 ;
+// }
+
+// // badgeBtn.addEventListener('click', badgeBtn);
